e3e65476b2688fdcbc1ff18f21fc9f87e3be597e
Sebastian Kralicek <kraliseb@proton.me>
2024-09-05 05:04:10 +0200

    nvim

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -1,61 +1,61 @@
-require('lazy').setup({
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+require("lazy").setup({
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} }, -- Use `opts = {}` to force a plugin to be loaded.
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} }, -- Use `opts = {}` to force a plugin to be loaded.
 
-  require 'kickstart/plugins/gitsigns',
+	require("kickstart/plugins/gitsigns"),
 
-  require 'kickstart/plugins/which-key',
+	require("kickstart/plugins/which-key"),
 
-  require 'kickstart/plugins/telescope',
+	require("kickstart/plugins/telescope"),
 
-  require 'kickstart/plugins/lspconfig',
+	require("kickstart/plugins/lspconfig"),
 
-  require 'kickstart/plugins/conform',
+	require("kickstart/plugins/conform"),
 
-  require 'kickstart/plugins/cmp',
+	require("kickstart/plugins/cmp"),
 
-  require 'kickstart/plugins/todo-comments',
+	require("kickstart/plugins/todo-comments"),
 
-  require 'kickstart/plugins/mini',
+	require("kickstart/plugins/mini"),
 
-  require 'kickstart/plugins/treesitter',
+	require("kickstart/plugins/treesitter"),
 
-  require 'kickstart.plugins.debug',
+	require("kickstart.plugins.debug"),
 
-  require 'kickstart.plugins.indent_line',
+	require("kickstart.plugins.indent_line"),
 
-  require 'kickstart.plugins.lint',
+	require("kickstart.plugins.lint"),
 
-  require 'kickstart.plugins.autopairs',
+	require("kickstart.plugins.autopairs"),
 
-  -- require 'kickstart.plugins.neo-tree',
+	-- require 'kickstart.plugins.neo-tree',
 
- --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom' },
-  { import = 'custom.plugins' },
+	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+	{ import = "custom" },
+	{ import = "custom.plugins" },
 }, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -13,12 +13,12 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -31,7 +31,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -47,11 +47,11 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 --  See `:help 'list'` and `:help 'listchars'`
-vim.opt.list = false  -- Disabled because of conflict with ibl
-vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
+vim.opt.list = false -- Disabled because of conflict with ibl
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -62,24 +62,23 @@ vim.opt.scrolloff = 10
 
 -- [[ My Options ]]
 
-
-vim.opt.foldmethod='expr'
-vim.opt.foldexpr='nvim_treesitter#foldexpr()'
-vim.opt.foldenable=false
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
 vim.opt.foldlevelstart = 99
 vim.opt.foldlevel = 99
 
 -- Configure line number colors
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#88B3F9' })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='#F38BA8' })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#88B3F9' })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#88B3F9" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#F38BA8" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#88B3F9" })
 
 -- Set cursor color for some modes
-vim.cmd('hi Cursor_insert guifg=#88B3F9 guibg=#88B3F9')
-vim.cmd('hi Cursor_replace guifg=#ED8796 guibg=#ED8796')
-vim.cmd('set guicursor=n-v-c-i:block,i-ci-ve:block-Cursor_insert/lCursor_insert,r:block-Cursor_replace/lCursor_replace')
+vim.cmd("hi Cursor_insert guifg=#88B3F9 guibg=#88B3F9")
+vim.cmd("hi Cursor_replace guifg=#ED8796 guibg=#ED8796")
+vim.cmd("set guicursor=n-v-c-i:block,i-ci-ve:block-Cursor_insert/lCursor_insert,r:block-Cursor_replace/lCursor_replace")
 
 -- Re-open at last position
-vim.cmd [[ au BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif ]]
+vim.cmd([[ au BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif ]])
 
 -- vim: ts=2 sts=2 sw=2 et
