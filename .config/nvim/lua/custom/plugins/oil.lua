d0bfb3998c16cd0c125b63e49f993ee373319653
Sebastian Kralicek <kraliseb@proton.me>
2024-10-06 04:08:52 +0200

    nvim

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,21 +1,35 @@
 return {
-    'stevearc/oil.nvim',
-    lazy = false,
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function ()
-        require('oil').setup {
-            default_file_explorer = true, -- Set to false if you still want to use netrw.
-            delete_to_trash = false, -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-            view_options = {
-                show_hidden = true,
-            },
-            float = {
-                max_width = 50,
-                max_height = 15,
-            },
-        }
-        vim.keymap.set('n', '<leader>o', function () require('oil').toggle_float(nil) end, { noremap = true, silent = true, desc = "Open floating file explorer"} )
-        vim.keymap.set('n', '-', ":Oil .<CR>", { noremap = true, silent = true, desc = "Open oil file explorer"} )
-    end,
+	"stevearc/oil.nvim",
+	lazy = false,
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			default_file_explorer = true, -- Set to false if you still want to use netrw.
+			delete_to_trash = false, -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+			keymaps = {
+				["<C-v>"] = {
+					"actions.select",
+					opts = { vertical = true },
+					desc = "Open the entry in a vertical split",
+				},
+				["<C-h>"] = {
+					"actions.select",
+					opts = { horizontal = true },
+					desc = "Open the entry in a horizontal split",
+				},
+			},
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				max_width = 50,
+				max_height = 15,
+			},
+		})
+		vim.keymap.set("n", "<leader>o", function()
+			require("oil").toggle_float(nil)
+		end, { noremap = true, silent = true, desc = "Open floating file explorer" })
+		vim.keymap.set("n", "-", ":Oil .<CR>", { noremap = true, silent = true, desc = "Open oil file explorer" })
+	end,
 }
