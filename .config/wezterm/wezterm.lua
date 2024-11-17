71787be08f8abb988223ae2902b964bb52e88b05
Sebastian Kralicek <kraliseb@proton.me>
2024-11-17 22:14:14 +0100

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,8 +8,41 @@ config.audible_bell = "Disabled"
 
 -- ====[[ APPEARANCE ]]====
 
-config.color_scheme = "Catppuccin Mocha (Gogh)"
--- config.color_scheme = "Monokai Pro (Gogh)"
+Colorschemes = {
+	"Catppuccin Mocha (Gogh)",
+	"Everforest Dark Hard (Gogh)",
+	"Kasugano (terminal.sexy)",
+	"Azu (Gogh)",
+	"duskfox",
+}
+
+Colorscheme_index = 1
+config.color_scheme = Colorschemes[Colorscheme_index]
+
+function Cycle_color_scheme(window, pane)
+	Colorscheme_index = Colorscheme_index + 1
+	if Colorscheme_index > #Colorschemes then
+		Colorscheme_index = 1
+	end
+
+	window:set_config_overrides({
+		colors = {
+			background = "black",
+		},
+		color_scheme = Colorschemes[Colorscheme_index],
+	})
+end
+
+wezterm.on("window-config-reloaded", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme then
+		print("Overides: " .. overrides.color_scheme)
+	end
+end)
+
+-- config.colors = {
+-- 	background = "#11111B",
+-- }
 
 config.initial_cols = 225
 config.initial_rows = 53
@@ -17,7 +50,7 @@ config.window_decorations = "NONE"
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 
 config.window_padding = {
 	left = 20,
@@ -38,6 +71,13 @@ local act = wezterm.action
 config.keys = {
 	-- paste from the clipboard
 	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{
+		key = "Enter",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			Cycle_color_scheme(window, pane)
+		end),
+	},
 }
 
 return config
