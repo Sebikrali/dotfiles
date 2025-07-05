8fbf904ea6165a6bd70e41c739250cd17a021970
Sebastian Kralicek <kraliseb@proton.me>
2025-07-05 22:22:09 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,68 @@
 #!/bin/bash
 
+######## DOTFILES ########
+
 # generate all directories
 dst=$HOME/.config
-dirs=$(ls $PWD/.config)
-
-for dir in $dirs; do
+config_dirs=$(ls $PWD/.config)
+for dir in $config_dirs; do
     mkdir -v $dst/$dir
 done
 
+# data_dirs=$(ls $PWD/.local/share)
+# for dir in $data_dirs; do
+#     mkdir -v $dst/$dir
+# done
+
 # Generate symlinks
-stow .
+stow .config
+stow .local
+stow bash
+stow zsh
+stow ideavim
 
-# Handle conflicts
+# Handle different hosts
+read -p "pc[1] or laptop[2]: " selection
+if [[ "$selection" == "1" || "$selection" == "pc" ]]; then
+    host="pc"
+elif [[ "$selection" == "2" || "$selection" == "laptop" ]]; then
+    host="laptop"
+else
+    echo "invalid host name. Terminating."
+    exit 1
+fi
 
-# read -p "pc or laptop: " comp
-#
-# dotfiles=$PWD
-# cd $dst/alacritty
-# ln -s "$dotfiles"/conflicts/"$comp"_alacritty.toml alacritty.toml
+host_dirs=$(ls $PWD/_$host)
+for dir in $host_dirs; do
+    files=$(ls $PWD/_$host/$dir)
+    for file in $files; do
+        mkdir -p $dst/$dir
+        ln -s -v $PWD/_$host/$dir/$file $dst/$dir/$file
+    done
+done
+
+
+######## PACKAGES ########
+
+# Source the os-release file
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Cannot determine the distribution."
+    exit 1
+fi
+
+case "$DISTRO" in
+    "arch")
+        sudo pacman -S --noconfirm  bat btop eza fastfetch fzf git-delta jq just lazygit mpd ncdu neovim nmon nvtop onefetch tokei ranger ripgrep rmpc starship stow syncthing sd tldr tmux trash-cli ufw wireguard-tools yazi zathura zip unzip zoxide zsh zsh-autosuggestions zsh-syntax-highlighting networkmanager flatpak gimp evince typst firefox go gparted mangohud obsidian rustup timeshift haruna wezterm
+        if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+            sudo pacman -S --noconfirm wl-clipboard
+        elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+            sudo pacman -S --noconfirm xclip xsel
+        fi
+        ;;
+    *)
+        ;;
+esac
 
